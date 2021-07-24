94528aece4b241ac544e3dbea23bb45783200ae5
dennis2311 <58165780+dennis2311@users.noreply.github.com>
2021-07-24 15:44:59 +0900

    user controller 기능 -> user service 이관

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -1,16 +1,6 @@
-import {
-    Body,
-    Controller,
-    Get,
-    NotFoundException,
-    Param,
-    Post,
-    Put,
-} from "@nestjs/common";
+import { Body, Controller, Get, Param, Post, Put } from "@nestjs/common";
 import { PrismaService } from "src/service/prisma.service";
 import { UserService } from "src/service/user.service";
-
-import { sendMail } from "../utils/sendMail";
 import { secretGenerator } from "../utils/secretGenerator";
 
 @Controller("user")
@@ -25,113 +15,20 @@ export class UserController {
         return this.userService.getUsers();
     }
 
-    @Get("list")
-    async getUser() {
-        const res = await this.prismaService.user.findMany();
-        return res;
-    }
-
-    // Eagloo-back의 userRouter.js.
-
     @Get(":email/:password")
     async login(
         @Param("email") email: string,
         @Param("password") password: string
     ) {
-        try {
-            const user = await this.prismaService.user.findUnique({
-                where: { email },
-            });
-            if (user) {
-                if (user.password === password) {
-                    if (user.authenticated) {
-                        if (!user.banned) {
-                            return {
-                                success: true,
-                            };
-                        } else {
-                            throw new NotFoundException(
-                                "신고로 인하여 정지된 계정입니다. 관리자에게 문의하세요"
-                            );
-                        }
-                    } else {
-                        throw new NotFoundException(
-                            "아직 계정 인증이 완료되지 않았어요"
-                        );
-                    }
-                } else {
-                    throw new NotFoundException("비밀번호가 일치하지 않아요");
-                }
-            } else {
-                throw new NotFoundException("일치하는 메일 주소가 없어요");
-            }
-        } catch (err) {
-            console.log(err);
-            throw new NotFoundException(
-                "서버 오류입니다. 잠시 후 다시 시도해 주세요"
-            );
-        }
+        return this.userService.login(email, password);
     }
 
     @Post()
     async signUpStep1(@Body() body) {
         const email = body.email;
         const secret = secretGenerator();
-        const response = { success: false, message: "" };
 
-        try {
-            const user = await this.prismaService.user.findUnique({
-                where: { email },
-            });
-            if (user) {
-                if (user.authenticated) {
-                    throw new NotFoundException(
-                        "이미 사용 중인 메일 주소입니다"
-                    );
-                } else {
-                    await this.prismaService.user.update({
-                        where: { email },
-                        data: { verificationSecret: secret },
-                    });
-                    // TODO
-                    // sendMail 발송 오류 처리
-                    if (sendMail(email, secret)) {
-                        return {
-                            success: true,
-                            message: `인증 메일이 ${email}@yonsei.ac.kr 로 발송되었습니다`,
-                        };
-                    } else {
-                        throw new NotFoundException(
-                            "메일 발송 중 오류가 발생했습니다"
-                        );
-                    }
-                }
-            } else {
-                await this.prismaService.user.create({
-                    data: {
-                        email,
-                        verificationSecret: secret,
-                    },
-                });
-                // TODO
-                // sendMail 발송 오류 처리
-                if (sendMail(email, secret)) {
-                    return {
-                        success: true,
-                        message: `인증 메일이 ${email}@yonsei.ac.kr 로 발송되었습니다`,
-                    };
-                } else {
-                    throw new NotFoundException(
-                        "메일 발송 중 오류가 발생했습니다"
-                    );
-                }
-            }
-        } catch (err) {
-            console.log(err);
-            throw new NotFoundException(
-                "서버 오류입니다. 잠시 후 다시 시도해 주세요"
-            );
-        }
+        return this.userService.signUp1(email, secret);
     }
 
     @Put("secret")
@@ -139,35 +36,7 @@ export class UserController {
         const email = body.email;
         const givenSecret = body.givenSecret;
 
-        try {
-            const user = await this.prismaService.user.findUnique({
-                where: { email },
-            });
-            if (user) {
-                if (user.verificationSecret === givenSecret) {
-                    await this.prismaService.user.update({
-                        where: { email },
-                        data: { authenticated: true },
-                    });
-                    return {
-                        success: true,
-                    };
-                } else {
-                    throw new NotFoundException(
-                        "인증 단어가 일치하지 않습니다"
-                    );
-                }
-            } else {
-                throw new NotFoundException(
-                    "임시 계정 생성이 완료되지 않았습니다. 잠시 후 다시 시도해주세요. \n증상이 계속되는 경우, 관리자에게 문의해주세요"
-                );
-            }
-        } catch (err) {
-            console.log(err);
-            throw new NotFoundException(
-                "서버 오류입니다. 잠시 후 다시 시도해 주세요"
-            );
-        }
+        return this.userService.signUp2(email, givenSecret);
     }
 
     @Put("password")
@@ -175,19 +44,6 @@ export class UserController {
         const email = body.email;
         const givenPassword = body.givenPassword;
 
-        try {
-            await this.prismaService.user.update({
-                where: { email },
-                data: { password: givenPassword },
-            });
-            return {
-                success: true,
-            };
-        } catch (err) {
-            console.log(err);
-            throw new NotFoundException(
-                "서버 오류입니다. 잠시 후 다시 시도해 주세요"
-            );
-        }
+        return this.userService.signUp3(email, givenPassword);
     }
 }
--- src/service/user.service.ts
+++ src/service/user.service.ts
@@ -1,5 +1,6 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, NotFoundException } from "@nestjs/common";
 import { PrismaClient } from "@prisma/client";
+import { sendMail } from "src/utils/sendMail";
 const prisma = new PrismaClient();
 
 @Injectable()
@@ -10,19 +11,167 @@ export class UserService {
             if (users) {
                 return users;
             }
-        } catch (error) {}
+        } catch (error) {
+            return null;
+        }
     }
 
     async login(email: string, password: string) {
         try {
             const user = await prisma.user.findUnique({
-                where: {
-                    email,
-                },
+                where: { email },
+            });
+            if (!user) {
+                return {
+                    success: false,
+                    errorMessage: "일치하는 메일 주소가 없어요.",
+                };
+            } else {
+                if (user.banned) {
+                    return {
+                        success: false,
+                        errorMessage: "신고로 인하여 정지된 계정입니다.",
+                    };
+                }
+                if (!user.authenticated) {
+                    return {
+                        success: false,
+                        errorMessage: "아직 인증이 완료되지 않은 계정입니다.",
+                    };
+                }
+                if (user.password !== password) {
+                    return {
+                        success: false,
+                        errorMessage: "비밀번호가 일치하지 않아요.",
+                    };
+                }
+                return {
+                    success: true,
+                    user: user,
+                };
+            }
+        } catch (err) {
+            console.error(err);
+            return {
+                success: false,
+                errorMessage: "서버 오류입니다. 잠시 후 다시 시도해 주세요.",
+            };
+        }
+    }
+
+    async signUp1(email: string, secret: string) {
+        try {
+            const user = await prisma.user.findUnique({
+                where: { email },
             });
             if (user) {
-                return user;
+                if (user.authenticated) {
+                    return {
+                        success: false,
+                        errorMessage: "이미 사용 중인 메일 주소입니다",
+                    };
+                } else {
+                    await prisma.user.update({
+                        where: { email },
+                        data: { verificationSecret: secret },
+                    });
+                    // TODO
+                    // sendMail 발송 오류 처리
+                    if (sendMail(email, secret)) {
+                        return {
+                            success: true,
+                            message: `인증 메일이 ${email}@yonsei.ac.kr 로 발송되었습니다`,
+                        };
+                    } else {
+                        return {
+                            success: false,
+                            errorMessage:
+                                "인증 메일 발송 중 오류가 발생했습니다",
+                        };
+                    }
+                }
             }
-        } catch (error) {}
+
+            await prisma.user.create({
+                data: {
+                    email,
+                    verificationSecret: secret,
+                },
+            });
+            // TODO
+            // sendMail 발송 오류 처리
+            if (sendMail(email, secret)) {
+                return {
+                    success: true,
+                    message: `인증 메일이 ${email}@yonsei.ac.kr 로 발송되었습니다`,
+                };
+            } else {
+                return {
+                    success: false,
+                    errorMessage: "인증 메일 발송 중 오류가 발생했습니다",
+                };
+            }
+        } catch (err) {
+            console.error(err);
+            return {
+                success: false,
+                errorMessage: "서버 오류입니다. 잠시 후 다시 시도해 주세요.",
+            };
+        }
+    }
+
+    async signUp2(email: string, givenSecret: string) {
+        try {
+            const user = await prisma.user.findUnique({
+                where: { email },
+            });
+            if (!user) {
+                return {
+                    success: false,
+                    errorMessage:
+                        "계정 생성이 완료되지 않았습니다. 잠시 후 처음부터 다시 시도해 주세요.",
+                };
+            }
+
+            if (user.verificationSecret !== givenSecret) {
+                return {
+                    success: false,
+                    errorMessage:
+                        "인증 단어가 일치하지 않습니다. 단어를 다시 확인해주세요. 인증 단어는 띄어쓰기를 포함합니다.",
+                };
+            }
+
+            await prisma.user.update({
+                where: { email },
+                data: { authenticated: true },
+            });
+            return {
+                success: true,
+            };
+        } catch (err) {
+            console.error(err);
+            return {
+                success: false,
+                errorMessage: "서버 오류입니다. 잠시 후 다시 시도해 주세요",
+            };
+        }
+    }
+
+    async signUp3(email: string, givenPassword: string) {
+        try {
+            await prisma.user.update({
+                where: { email },
+                data: { password: givenPassword },
+            });
+            return {
+                success: true,
+            };
+        } catch (err) {
+            console.error(err);
+            return {
+                success: false,
+                errorMessage: "서버 오류입니다. 잠시 후 다시 시도해 주세요",
+            };
+        }
     }
 }
