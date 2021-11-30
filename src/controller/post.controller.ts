155b3bbd7a3e82f965b2873b867ac91ab6ea231b
julysein723 <julysein723@gmail.com>
2021-11-30 13:06:30 +0900

    feat : PostBoard api

--- src/controller/post.controller.ts
+++ src/controller/post.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Post, Body } from "@nestjs/common";
+import {
+    Body,
+    Controller,
+    Delete,
+    Get,
+    Param,
+    Post,
+    Put,
+} from "@nestjs/common";
 import { ApiTags } from "@nestjs/swagger";
 import { PostService } from "src/service";
 import { PostCategory } from ".prisma/client";
@@ -7,6 +15,16 @@ import { PostCategory } from ".prisma/client";
 @Controller("post")
 export class PostController {
     constructor(private readonly postService: PostService) {}
+
+    @Get(":roomId")
+    async getAllPosts(@Param("roomId") roomId: string) {
+        return this.postService.getAllPosts(roomId);
+    }
+
+    @Get(":postId")
+    async getPost(@Param("postId") postId: string) {
+        return this.postService.getPost(postId);
+    }
 
     @Post()
     async createPost(@Body() body) {
@@ -23,5 +41,84 @@ export class PostController {
             postContents,
             category
         );
+    }
+
+    @Post()
+    async createPostLike(@Body() body) {
+        const userId: string = body.userId;
+        const postId: string = body.postId;
+
+        return this.postService.createPostLike(
+            userId,
+            postId,
+        );
+    }
+
+    @Post()
+    async createPostScrap(@Body() body) {
+        const userId: string = body.userId;
+        const postId: string = body.postId;
+
+        return this.postService.createPostScrap(
+            userId,
+            postId,
+        );
+    }
+
+    @Post()
+    async createPostComment(@Body() body) {
+        const userId: string = body.userId;
+        const postId: string = body.postId;
+        const comment: string = body.comment;
+
+        return this.postService.createPostComment(
+            userId,
+            postId,
+            comment
+        );
+    }
+
+    @Put(":postId")
+    async updatePost(@Body() body) {
+        const postId: string = body.postId;
+        const postTitle: string = body.postTitle;
+        const postContents: string = body.postContents;
+
+        return this.postService.updatePost(
+            postId,
+            postTitle,
+            postContents
+        );
+    }
+
+    @Put(":postCommentId")
+    async updatePostComment(@Body() body) {
+        const postCommentId : string = body.postCommentId;
+        const comment: string = body.comment;
+
+        return this.postService.updatePostComment(
+            postCommentId,
+            comment,
+        );
+    }
+
+    @Delete(":postId")
+    async deletePost(@Param("postId") postId: string) {
+        return this.postService.deletePost(postId);
+    }
+
+    @Delete(":postLikeId")
+    async deletePostLike(@Param("postLikeId") postLikeId: string) {
+        return this.postService.deletePostLike(postLikeId);
+    }
+
+    @Delete(":postScrapId")
+    async deletePostScrapLike(@Param("postScrapId") postScrapId: string) {
+        return this.postService.deletePostScrapLike(postScrapId);
+    }
+
+    @Delete(":postCommentId")
+    async deletePostCommentLike(@Param("postCommentId") postCommentId: string) {
+        return this.postService.deletePostCommentLike(postCommentId);
     }
 }
--- src/service/post.service.ts
+++ src/service/post.service.ts
@@ -7,6 +7,106 @@ const prisma = new PrismaClient();
 export class PostService {
     constructor() {}
 
+    async getAllPosts(roomId: string) {
+        try {
+            const allPosts = await prisma.customRoom.findUnique({
+                where: {
+                    id : roomId,
+                },
+                select: {
+                    posts : {
+                        select : {
+                            id: true,
+                            category: true,
+                            title: true,
+                            contents: true,
+                            authorId : true,
+                            roomId : true,
+                            postlikes : {
+                                select : {
+                                    userId : true,
+                                },
+                            },
+                            postScraps : {
+                                select : {
+                                    userId : true,
+                                },
+                            },
+                            postComments : {
+                                select : {
+                                    userId : true,
+                                },
+                            },
+                            createdAt : true,
+                            updatedAt : true,
+                        },
+                    },
+                },
+            });
+            return {
+                success: true,
+                posts: allPosts,
+            };
+        } catch (error) {
+            console.error(error);
+            return {
+                success: false,
+                message: "서버 오류입니다. 잠시 후 다시 시도해 주세요",
+            };
+        }
+    }
+
+    async getPost(postId: string) {
+        try {
+            const post = await prisma.post.findUnique({
+                where: {
+                    id : postId,
+                },
+                select: {
+                    id: true,
+                    category: true,
+                    title: true,
+                    contents: true,
+                    authorId : true,
+                    roomId : true,
+                    postlikes : {
+                        select : {
+                            id : true,
+                            userId : true,
+                        },
+                    },
+                    postScraps : {
+                        select : {
+                            id : true,
+                            userId : true,
+                        },
+                    },
+                    postComments : {
+                        select : {
+                            id : true,
+                            userId : true,
+                            comment : true,
+                            createdAt : true,
+                            updatedAt : true,
+                        },
+                    },
+                    createdAt : true,
+                    updatedAt : true,
+                },
+            });
+            return {
+                success: true,
+                post: post,
+            };
+        } catch (error) {
+            console.error(error);
+            return {
+                success: false,
+                message: "서버 오류입니다. 잠시 후 다시 시도해 주세요",
+            };
+        }
+    }
+
     async createPost(
         userId: string,
         roomId: string,
@@ -28,6 +128,184 @@ export class PostService {
             return { success: true };
         } catch (error) {
             return { success: false, message: "포스트 생성에 실패했습니다." };
+        }
+    }
+
+    async createPostLike(
+        userId: string,
+        postId: string,
+    ) {
+        try {
+            await prisma.postLike.create({
+                data: {
+                    userId,
+                    postId
+                },
+            });
+
+            return { success: true };
+        } catch (error) {
+            return { success: false, message: "포스트 생성에 실패했습니다." };
+        }
+    }
+
+    async createPostScrap(
+        userId: string,
+        postId: string,
+    ) {
+        try {
+            await prisma.postScrap.create({
+                data: {
+                    userId,
+                    postId
+                },
+            });
+
+            return { success: true };
+        } catch (error) {
+            return { success: false, message: "포스트 생성에 실패했습니다." };
+        }
+    }
+
+    async createPostComment(
+        userId: string,
+        postId: string,
+        comment : string,
+    ) {
+        try {
+            await prisma.postComment.create({
+                data: {
+                    userId,
+                    postId,
+                    comment
+                },
+            });
+
+            return { success: true };
+        } catch (error) {
+            return { success: false, message: "포스트 생성에 실패했습니다." };
+        }
+    }
+
+    async updatePost(
+        postId : string,
+        postTitle: string,
+        postContents: string
+    ) {
+        try {
+            await prisma.post.update({
+                where: {
+                    id: postId,
+                },
+                data: {
+                    title : postTitle,
+                    contents : postContents,
+                },
+            });
+
+            return { success: true };
+        } catch (error) {
+            console.error(error);
+            return {
+                success: false,
+                message: "서버 오류입니다. 잠시 후 다시 시도해 주세요",
+            };
+        }
+    }
+
+    async updatePostComment(
+        postCommentId : string,
+        comment : string,
+    ) {
+        try {
+            await prisma.postComment.update({
+                where: {
+                    id: postCommentId,
+                },
+                data: {
+                    comment,
+                },
+            });
+
+            return { success: true };
+        } catch (error) {
+            console.error(error);
+            return {
+                success: false,
+                message: "서버 오류입니다. 잠시 후 다시 시도해 주세요",
+            };
+        }
+    }
+
+    async deletePost(postId: string) {
+        try {
+            await prisma.post.delete({
+                where: {
+                    id: postId,
+                },
+            });
+
+            return { success: true };
+        } catch (error) {
+            console.error(error);
+            return {
+                success: false,
+                message: "서버 오류입니다. 잠시 후 다시 시도해 주세요",
+            };
+        }
+    }
+
+    async deletePostLike(postLikeId: string) {
+        try {
+            await prisma.postLike.delete({
+                where: {
+                    id: postLikeId,
+                },
+            });
+
+            return { success: true };
+        } catch (error) {
+            console.error(error);
+            return {
+                success: false,
+                message: "서버 오류입니다. 잠시 후 다시 시도해 주세요",
+            };
+        }
+    }
+
+    async deletePostScrapLike(postScrapId: string) {
+        try {
+            await prisma.postScrap.delete({
+                where: {
+                    id: postScrapId,
+                },
+            });
+
+            return { success: true };
+        } catch (error) {
+            console.error(error);
+            return {
+                success: false,
+                message: "서버 오류입니다. 잠시 후 다시 시도해 주세요",
+            };
+        }
+    }
+
+    async deletePostCommentLike(postCommentId: string) {
+        try {
+            await prisma.postComment.delete({
+                where: {
+                    id: postCommentId,
+                },
+            });
+
+            return { success: true };
+        } catch (error) {
+            console.error(error);
+            return {
+                success: false,
+                message: "서버 오류입니다. 잠시 후 다시 시도해 주세요",
+            };
         }
     }
 }
